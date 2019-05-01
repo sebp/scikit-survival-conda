17920cfa1ec2a5d52fc815a9b4e60693d5427e01
Sebastian Pölsterl <sebp@k-d-w.org>
2019-05-01 17:34:47 +0200

    rename conda's default enviornment from root to base

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -2,14 +2,14 @@
 set -ve
 
 curl -fsSL https://raw.githubusercontent.com/pelson/Obvious-CI/master/bootstrap-obvious-ci-and-miniconda.py > bootstrap-obvious-ci-and-miniconda.py
-python bootstrap-obvious-ci-and-miniconda.py ~/miniconda x64 3 --without-obvci && source ~/miniconda/bin/activate root
+python bootstrap-obvious-ci-and-miniconda.py ~/miniconda x64 3 --without-obvci && source ~/miniconda/bin/activate base
 
 conda config --set show_channel_urls true
 conda config --set add_pip_as_python_dependency false
 conda config --add channels sebp
 
-conda update -n root --yes --quiet conda conda-env
-conda install -n root --yes --quiet anaconda-client conda-build setuptools
+conda update -n base -c defaults --yes --quiet conda
+conda install -n base --yes --quiet anaconda-client conda-build setuptools
 
 conda info
 conda config --get
