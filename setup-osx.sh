0f7f1ce73d9fb87925ec79c003000d72fdca0ca3
Sebastian Pölsterl <sebp@k-d-w.org>
2022-04-04 17:42:34 +0200

    Build conda package for osx-arm64

--- setup-osx.sh
+++ setup-osx.sh
@@ -9,14 +9,18 @@ export CPU_COUNT=2
 
 export PYTHONUNBUFFERED=1
 
-export MACOSX_DEPLOYMENT_TARGET=10.9
+export MACOSX_DEPLOYMENT_TARGET='${1:-10.9}'
 
 export CONDA_BUILD_SYSROOT="$(xcode-select -p)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk"
 
 if [[ ! -d ${CONDA_BUILD_SYSROOT} || "$OSX_FORCE_SDK_DOWNLOAD" == "1" ]]; then
     echo "Downloading ${MACOSX_DEPLOYMENT_TARGET} sdk"
-    curl -L -O https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk.tar.xz
-    tar -xf MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk.tar.xz -C "$(dirname "$CONDA_BUILD_SYSROOT")"
+    curl -L -O https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz
+    mkdir -p "$(dirname "$CONDA_BUILD_SYSROOT")"
+    tar -xf MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz -C "$(dirname "$CONDA_BUILD_SYSROOT")"
+fi
+
+if [[ "${MACOSX_DEPLOYMENT_TARGET}" == 10.* && "${USING_SYSTEM_SDK_DIR:-}" == "1" ]]; then
     # set minimum sdk version to our target
     plutil -replace MinimumSDKVersion -string ${MACOSX_DEPLOYMENT_TARGET} $(xcode-select -p)/Platforms/MacOSX.platform/Info.plist
     plutil -replace DTSDKName -string macosx${MACOSX_DEPLOYMENT_TARGET}internal $(xcode-select -p)/Platforms/MacOSX.platform/Info.plist
